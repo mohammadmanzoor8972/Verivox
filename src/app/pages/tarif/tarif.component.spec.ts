3fd92720a64543de4df56f46548ccfc97d58cf01
mohammad.manzoor8972@gmail.com <malam@LKNDSS0NRG3QD.local>
2020-01-21 02:00:12 +0530

    Fixed test cases

--- src/app/pages/tarif/tarif.component.spec.ts
+++ src/app/pages/tarif/tarif.component.spec.ts
@@ -1,15 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TarifComponent } from './tarif.component';
-
+import { TarifItemComponent } from 'src/app/components/tarif-item/tarif-item.component';
+import { TarifUploadDownloadComponent } from 'src/app/components/tarif-upload-download/tarif-upload-download.component';
+import { HttpClientModule } from '@angular/common/http';
 describe('TarifComponent', () => {
   let component: TarifComponent;
   let fixture: ComponentFixture<TarifComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TarifComponent ]
+      declarations: [   TarifItemComponent,
+        TarifComponent,
+        TarifUploadDownloadComponent],
+        imports: [HttpClientModule]
     })
+
     .compileComponents();
   }));
 
@@ -18,5 +23,17 @@ describe('TarifComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+
+
+  it('should create the app', () => {
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should create the app', () => {
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
 
 });
